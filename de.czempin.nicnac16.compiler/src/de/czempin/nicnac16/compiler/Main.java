41aae0edbb93b92979a4f4e30e02dea103ba6ad1
Nicolai Czempin <contact@czempin.de>
2014-10-05 21:19:06 +0200

    precompile the pattern matcher

--- de.czempin.nicnac16.compiler/src/de/czempin/nicnac16/compiler/Main.java
+++ de.czempin.nicnac16.compiler/src/de/czempin/nicnac16/compiler/Main.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.util.StringTokenizer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.io.Files;
@@ -16,13 +18,22 @@ public class Main {
 		File file = new File("test001.c");
 		Charset charset = Charset.defaultCharset();
 		ImmutableList<String> lines = Files.asCharSource(file, charset).readLines();
-
+		Pattern p = Pattern.compile("^\\w.*");
 		for (String line : lines) {
 			StringTokenizer st = new StringTokenizer(line);
 			String token = null;
 			while (st.hasMoreTokens()) {
 				token = st.nextToken().trim();
-				System.out.println("-"+token);
+				
+				 Matcher m = p.matcher(token);
+				
+				if (m.matches()){
+					 // do something
+					 System.out.println(token +" starts with a word");
+				 } else {
+						System.out.println(token+" is not a word");				 
+				 }
+				
 			}
 		}
 
