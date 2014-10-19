5401c09e193ee561f2bcdfa472fb602af14159bd
Nicolai Czempin <contact@czempin.de>
2014-10-20 00:26:43 +0200

    added more syntactic elements; shuffled around the dummy data

--- de.czempin.nicnac16.compiler/src/de/czempin/nicnac16/compiler/Statement.java
+++ de.czempin.nicnac16.compiler/src/de/czempin/nicnac16/compiler/Statement.java
@@ -1,40 +1,10 @@
 package de.czempin.nicnac16.compiler;
 
 public class Statement {
-	private int counter = 0;
 
 	@Override
 	public String toString() {
-		String retVal = "    ";
-		switch (counter) {
-		case 0:
-			retVal += "int A;"; //declaration
-			break;
-		case 1:
-			retVal += "int _b;"; //declaration
-			break;
-		case 2:
-			retVal += "int c7;"; //declaration
-			break;
-		case 3:
-			retVal += "int d;"; //declaration
-			break;
-		case 4:
-			retVal += "A = 7;"; //assignment
-			break;
-		case 5:
-			retVal += "_b = 25;"; //assignment
-			break;
-		case 6:
-			retVal += "c7 = A + _b;"; //assignment (expression on rvalue)
-			break;
-		case 7:
-			retVal += "return c7;"; //return_statement
-			break;
-		}
-		retVal += "\n";
-		++counter;
-		return retVal;
+				return "UNKNOWN_STATEMENT\n";
 	}
 
 }
